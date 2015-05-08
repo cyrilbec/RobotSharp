18f324fed43816c4f24eb10b644fd6aec49d361c
becbec2 <cyrilbec@gmail.com>
2015-05-08 21:23:39 +0200

    create web server project

---
+++ RobotSharp.WebServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace RobotSharp.WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:81")))
+            {
+                host.Start();
+                
+                Console.WriteLine("Press any key to quit.");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
