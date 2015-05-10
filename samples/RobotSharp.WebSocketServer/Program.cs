2b4d507babfdf1bfd8702b479396eb8fdeebe8a3
becbec2 <cyrilbec@gmail.com>
2015-05-10 22:58:51 +0200

    web socket server project creation

---
+++ samples/RobotSharp.WebSocketServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RobotSharp.WebSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketSharp.Server.WebSocketServer(81);
+            wssv.AddWebSocketService<WebSocketMessageReceiver>("/gpioport");
+            wssv.Start();
+
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
---
+++ samples/RobotSharp.WebSocketServer/WebSocketMessageReceiver.cs
@@ -0,0 +1,13 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace RobotSharp.WebSocketServer
+{
+    public class WebSocketMessageReceiver : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            
+        }
+    }
+}
